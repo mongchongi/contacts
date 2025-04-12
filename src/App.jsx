ba8a04e4a286536d8db4aa43634821cdae6792dd
mongchongi <yeonguk0517@gmail.com>
2025-04-12 10:56:15 +0900

    기본 레이아웃 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Grid } from '@mui/material';
+import './App.css';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='contacts'>
+      <h1 className='contacts__title'>연락처 앱</h1>
+      <Grid container spacing={2}>
+        <Grid size={6}>item 1</Grid>
+        <Grid size={6}>item 2</Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default App;

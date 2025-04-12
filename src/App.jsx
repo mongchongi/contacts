ce24b9427aa7f0c33a5c6759aa9f4461352d166e
mongchongi <yeonguk0517@gmail.com>
2025-04-12 13:34:00 +0900

    스타일 위치 변경 & 모달 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,77 @@
-import { alpha, Box, Grid, InputBase, styled, AppBar, Toolbar, Typography } from '@mui/material';
+import { alpha, Box, Grid, InputBase, styled, AppBar, Toolbar, Typography, Button, Modal } from '@mui/material';
 import SearchIcon from '@mui/icons-material/Search';
+import AddCallIcon from '@mui/icons-material/AddCall';
 import './App.css';
 import ContactForm from './components/ContactForm';
 import ContactsList from './components/ContactsList';
+import { useState } from 'react';
+import useContactsStore from './stores/useContactsStore';
+
+const App = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const contacts = useContactsStore((state) => state.contacts);
+
+  const handleSearch = (event) => {};
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position='static'>
+          <Toolbar>
+            <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
+              연락처 앱
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} onChange={handleSearch} />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Grid container spacing={2}>
+        <Grid size={6} display={{ xs: 'none', sm: 'block' }} padding={'20px 0 0 20px'}>
+          <ContactForm />
+        </Grid>
+        <Grid size={{ xs: 12, sm: 6 }}>
+          <ContactsList />
+        </Grid>
+      </Grid>
+
+      <Button
+        variant='contained'
+        sx={{
+          position: 'fixed',
+          right: '20px',
+          bottom: '20px',
+          display: { xs: 'flex', sm: 'none' },
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+        onClick={handleOpen}
+      >
+        <AddCallIcon />
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box sx={style}>
+          <ContactForm />
+        </Box>
+      </Modal>
+    </>
+  );
+};
+
+export default App;
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -45,35 +114,13 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
   },
 }));
 
-const App = () => {
-  return (
-    <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position='static'>
-          <Toolbar>
-            <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}>
-              연락처 앱
-            </Typography>
-            <Search>
-              <SearchIconWrapper>
-                <SearchIcon />
-              </SearchIconWrapper>
-              <StyledInputBase placeholder='Search…' inputProps={{ 'aria-label': 'search' }} />
-            </Search>
-          </Toolbar>
-        </AppBar>
-      </Box>
-
-      <Grid container spacing={2}>
-        <Grid size={6} display={{ xs: 'none', sm: 'block' }}>
-          <ContactForm />
-        </Grid>
-        <Grid size={{ xs: 12, sm: 6 }}>
-          <ContactsList />
-        </Grid>
-      </Grid>
-    </div>
-  );
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '90%',
+  bgcolor: 'background.paper',
+  p: 4,
+  borderRadius: '5px',
 };
-
-export default App;
